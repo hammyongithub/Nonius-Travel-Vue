4c18d81c95b8c8a36f3040debc9608748d12f981
Jorge Domingues <jorge@domingues.tech>
2023-11-30 17:15:07 +0000

    Router GUard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useStore } from '../store'; // Adjust the path to your Pinia store as needed
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,29 +23,50 @@ const router = createRouter({
     {
       path: '/settings',
       name: 'settings',
-      component: () => import('../views/Settings.vue')
+      component: () => import('../views/Settings.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('../views/Dashboard.vue')
+      component: () => import('../views/Dashboard.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/customers',
       name: 'customers',
-      component: () => import('../views/Customers.vue')
+      component: () => import('../views/Customers.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/bookings',
       name: 'bookings',
-      component: () => import('../views/Bookings.vue')
+      component: () => import('../views/Bookings.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/packages',
       name: 'packages',
-      component: () => import('../views/Packages.vue')
+      component: () => import('../views/Packages.vue'),
+      meta: { requiresAuth: true }
     },
+    // ... any other routes
   ]
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const store = useStore(); // Use your Pinia store
+
+  // Check if the route requires authentication
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !store.isLoggedIn) {
+    // If the user is not logged in, redirect to the login page
+    next({ name: 'login' });
+  } else {
+    // Otherwise, proceed as normal
+    next();
+  }
+});
+
+export default router;
